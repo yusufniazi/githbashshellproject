ad81d671b44527a2d7dbfa9294ece1c641d7eb8f
yusuf Niazi <yusufdeveloper920@gmail.com>
2025-04-20 06:55:25 +0500

    added file

---
+++ day2/day2learn.sh
@@ -0,0 +1,14 @@
+#!bin/bash 
+
+# Learn Bash script basic to Advance  
+
+# for multiple Comment 
+ # varible =  container 
+
+name="Yusuf Khan"
+echo "Hello : $name"
+# echo "Hello : $name"
+# echo for Date command 
+echo " Hello , $name today is your date: $(date)"
+
+
